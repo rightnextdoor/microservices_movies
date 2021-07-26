62dc0fbc5d6f80a0fc0c48a047e649eb9d3d4204
Kevin Pratt <redhotkbp@hotmail.com>
2021-07-26 10:41:08 -0500

    inital commit

---
+++ movie-catelog-service/movie-catelog-service/src/test/java/com/movies/moviecatelogservice/MovieCatelogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.moviecatelogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatelogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
